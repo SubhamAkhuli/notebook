a58a34eea8502df46de903b19e3ca3326cbdc22b
SubhamAkhuli <subhamakhuli7586@gmail.com>
2024-02-22 21:30:22 +0530

    Add noteContext.js to handle notes context

---
+++ src/context/notes/noteContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+
+// createContext is a function that creates a context to be used in the app. It takes a default value as an argument.
+const NoteContext = createContext();
+export default NoteContext;
