1337df7471cbcf2e6983394c6ab33df2c2f3e7ff
SubhamAkhuli <subhamakhuli7586@gmail.com>
2024-02-22 21:30:46 +0530

    Add React Router and Navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+// new react router process  import  createBrowserRouter, RouterProvider from react-router-dom
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NoteState from "./context/notes/NoteState";
+
+// new react router process  create a router
+const router = createBrowserRouter([
+  {
+    // path is the url path
+    path: "/",
+    // element is the component to be rendered
+    element: <>
+      <Navbar title="NoteBook" about="About" />
+      <Home />
+    </>,
+  },
+  {
+    path: "/about",
+    element: <>
+      <Navbar title="NoteBook" about="About" />
+      <About />
+    </>,
+  }]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
+    <>
+    {/* NoteState set the range of the context  */}
+      <NoteState>
+      {/* to call the pages */}
+      <RouterProvider router={router} />
+      </NoteState>
+    </>
   );
 }
 
