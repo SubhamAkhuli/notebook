a4c7b8d92e0ac5cbac7f042f94e946f89459df37
SubhamAkhuli <subhamakhuli7586@gmail.com>
2024-02-22 12:19:13 +0530

    Add About component to web notebook

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className='container'>
+            <h1>About</h1>
+            <p>Welcome to our web notebook! This is a place where you can jot down your thoughts, ideas, and notes in a digital format. Whether you're a student, professional, or just someone who loves to stay organized, our web notebook is here to help you stay productive.</p>
+            <p>With our user-friendly interface, you can create, edit, and organize your notes with ease. You can also categorize your notes using tags, making it easier to find what you're looking for.</p>
+            <p>Feel free to explore the various features of our web notebook and make the most out of your note-taking experience. We hope you find our web notebook useful and enjoy using it!</p>
+            <p>Happy note-taking!</p>
+            
+        </div>
+    )
+}
