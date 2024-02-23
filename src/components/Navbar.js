cc10805625a66ed93895177a9eabb3a9a465518a
SubhamAkhuli <subhamakhuli7586@gmail.com>
2024-02-23 13:19:28 +0530

    Update Navbar component to use NavLink instead of Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-// link is used to navigate to different pages in the app without refreshing the page.
-import { Link } from "react-router-dom";
+
+// NavLink/Link is used to navigate to different pages in the app without refreshing the page.But we use NavLink becasue also it is used to style the active link to get highlight in navbar.
+import { NavLink } from "react-router-dom";
+
 
 export default function Navbar(props) {
   return (
     <div>
       {/* navbar component */}
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <NavLink className="navbar-brand" to="/">
             {props.title}
-          </Link>
+          </NavLink>
           <button
             className="navbar-toggler"
             type="button"
@@ -26,14 +28,14 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <NavLink className="nav-link" aria-current="page" to="/">
                   Home
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <NavLink className="nav-link" to="/about">
                   {props.about}
-                </Link>
+                </NavLink>
               </li>
             </ul>
           </div>
