9324be2d38f5a5945b5a6a77b8dac763a027fe64
SubhamAkhuli <subhamakhuli7586@gmail.com>
2024-02-26 21:01:31 +0530

    Add useState hook and showAlert function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -7,48 +7,73 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NoteState from "./context/notes/NoteState";
 import Login from "./components/User/Login";
 import Signup from "./components/User/Signup";
-// import Alert from "./components/Alert";
+import Alert from "./components/Alert";
 
-// new react router process  create a router
-const router = createBrowserRouter([
-  {
-    // path is the url path
-    path: "/",
-    // element is the component to be rendered
-    element: <>
-      <Navbar title="NoteBook" about="About" />
-      {/* <Alert message="Success! Your note has been added successfully."/> */}
-      <Home />
-    </>,
-  },
-  {
-    path: "/about",
-    element: <>
-      <Navbar title="NoteBook" about="About" />
-      <About />
-    </>,
-  },
-  {
-    path: "/signup",
-    element: <>
-      <Navbar title="NoteBook" about="About" />
-      <Signup />
-    </>,
-  },
-  {
-    path: "/login",
-    element: <>
-      <Navbar title="NoteBook" about="About" />
-      <Login />
-    </>,
-  }]);
+
 
 function App() {
+
+  // show alert
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  // new react router process  create a router
+  const router = createBrowserRouter([
+    {
+      // path is the url path
+      path: "/",
+      // element is the component to be rendered
+      element: <>
+        <Navbar title="NoteBook" about="About" />
+        <Alert alert={alert} />
+        <Home showAlert={showAlert} />
+
+      </>,
+    },
+    {
+      path: "/about",
+      element: <>
+        <Navbar title="NoteBook" about="About" />
+        <About />
+      </>,
+    },
+    {
+      path: "/signup",
+      element: <>
+
+        <Navbar title="NoteBook" about="About" />
+        <Alert alert={alert} />
+        <Signup showAlert={showAlert} />
+
+      </>,
+    },
+    {
+      path: "/login",
+      element: <>
+
+        <Navbar title="NoteBook" about="About" />
+        <Alert alert={alert} />
+        <Login showAlert={showAlert} />
+
+      </>,
+    }]);
+
   return (
     <>
       {/* set the range of the context  */}
       <NoteState>
         {/* context available from here to */}
+
+
         {/* to call the pages */}
         <RouterProvider router={router} />
         {/* here */}
