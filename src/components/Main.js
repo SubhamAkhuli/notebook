013b7e5499c71c959215a7db4a776f4db96b329d
SubhamAkhuli <subhamakhuli7586@gmail.com>
2024-03-01 12:04:44 +0530

    Add Main component to display welcome message and instructions

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div className='container'>
+            <h1 >Welcome to the NoteBook</h1>
+            <p className='mt-4'>Frist you need to create an account, Then you can add your note and update and delete it.</p>
+            <p>Click on the <strong>signup</strong> button to create an account.</p>
+            <p>Click on the <strong>Add Note</strong> button to add your note.</p>
+            <p>Click on the <strong>Update</strong> icon to update your note.</p>
+            <p>Click on the <strong>Delete</strong> icon to delete your note.</p>
+        </div>
+    );
+}
+export default Main
