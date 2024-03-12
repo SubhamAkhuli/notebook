d0a55e114816aab79c02993c175a122c2ef905bf
SubhamAkhuli <subhamakhuli7586@gmail.com>
2024-03-12 20:58:19 +0530

    Refactor App.js to use context and add user context

--- src/App.js
+++ src/App.js
@@ -4,28 +4,29 @@ import Home from "./components/Home";
 import About from "./components/About";
 // new react router process  import  createBrowserRouter, RouterProvider from react-router-dom
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import the note context to use the context    
 import NoteState from "./context/notes/NoteState";
+// import the user context to use the context 
+import UserState from "./context/user/UserState";
 import Login from "./components/User/Login";
 import Signup from "./components/User/Signup";
 import Alert from "./components/Alert";
 import Main from "./components/Main";
 import Profile from "./components/User/Profile";
 
-
 function App() {
-
   // show alert
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
       setAlert(null);
     }, 1500);
-  }
+  };
 
   // new react router process  create a router
   const router = createBrowserRouter([
@@ -33,76 +34,80 @@ function App() {
       // path is the url path
       path: "/home",
       // element is the component to be rendered
-      element: <>
-        <Navbar title="NoteBook" about="About" />
-        <Alert alert={alert} />
-        <Home showAlert={showAlert} />
-
-      </>,
+      element: (
+        <>
+          <Navbar title="NoteBook" about="About" />
+          <Alert alert={alert} />
+          <Home showAlert={showAlert} />
+        </>
+      ),
     },
     {
       path: "/about",
-      element: <>
-        <Navbar title="NoteBook" about="About" />
-        <About />
-      </>,
+      element: (
+        <>
+          <Navbar title="NoteBook" about="About" />
+          <About />
+        </>
+      ),
     },
     {
       path: "/signup",
-      element: <>
-
-        <Navbar title="NoteBook" about="About" />
-        <Alert alert={alert} />
-        <Signup showAlert={showAlert} />
-
-      </>,
+      element: (
+        <>
+          <Navbar title="NoteBook" about="About" />
+          <Alert alert={alert} />
+          <Signup showAlert={showAlert} />
+        </>
+      ),
     },
     {
       path: "/login",
-      element: <>
-
-        <Navbar title="NoteBook" about="About" />
-        <Alert alert={alert} />
-        <Login showAlert={showAlert} />
-
-      </>,
+      element: (
+        <>
+          <Navbar title="NoteBook" about="About" />
+          <Alert alert={alert} />
+          <Login showAlert={showAlert} />
+        </>
+      ),
     },
     {
       // path is the url path
       path: "/",
       // element is the component to be rendered
-      element: <>
-        <Navbar title="NoteBook" about="About" />
-        <Alert alert={alert} />
-        <Main/>
-
-      </>,
-    }
-    ,
+      element: (
+        <>
+          <Navbar title="NoteBook" about="About" />
+          <Alert alert={alert} />
+          <Main />
+        </>
+      ),
+    },
     {
       path: "/profile",
-      element: <>
-
-        <Navbar title="NoteBook" about="About" />
-        <Alert alert={alert} />
-        <Profile showAlert={showAlert} />
-
-      </>,
-    }]);
+      element: (
+        <>
+          <Navbar title="NoteBook" about="About" />
+          <Alert alert={alert} />
+          <Profile showAlert={showAlert} />
+        </>
+      ),
+    },
+  ]);
 
   return (
     <>
       {/* set the range of the context  */}
       <NoteState>
         {/* context available from here to */}
+        <UserState>
 
+          {/* to call the pages */}
+          <RouterProvider router={router} />
 
-        {/* to call the pages */}
-        <RouterProvider router={router} />
-        
+        </UserState>
         {/* here */}
       </NoteState>
-      
     </>
   );
 }
